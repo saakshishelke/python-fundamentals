85447a6a2f2e2a0dc589831686fdf98e01d028dc
saakshishelke <saakshi.shelke@gmail.com>
2026-06-05 18:36:56 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,10 @@
+name=input("Enter your name:")
+age=input("Enter your age:")
+city=input("Enter Your City:")
+dream_company=input("Enter your Dream Company:")
+
+print("hello", name)
+print("Your are", age,"Years old")
+print("You are from",city)
+print("your dream company is", dream_company)
+
