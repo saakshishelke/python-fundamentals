52f830b87bf891cbfbfe68ff15ae5aed85ab8d23
saakshishelke <saakshi.shelke@gmail.com>
2026-06-06 13:26:55 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,9 @@
+username=input("Enter Your Username:")
+password=input("Enter your Password:")
+
+if username=="Sakshi"and password=="Google#2026":
+    print("login Succesful")
+
+else:
+    print("Somthing is wrong ")
+
