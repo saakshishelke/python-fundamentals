d87e74386844e0669bcdb7e3fb02100d0c464924
saakshishelke <saakshi.shelke@gmail.com>
2026-06-06 13:28:47 +0530

    Add files via upload

---
+++ calculator_function.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    return a+b
+
+def sub(a, b):
+    return a-b
+
+def mul(a, b):
+    return a*b
+
+def div(a, b):
+    return a/b
+
+print(add(5,3))
+print(sub(5,3))
+print(mul(5,3))
+print(div(5,3))
+
+
+
