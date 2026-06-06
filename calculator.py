07e1aade29ba11dd407e7fb58687a3b8959ac779
saakshishelke <saakshi.shelke@gmail.com>
2026-06-06 13:25:48 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,10 @@
+number1=int(input("Enter First Number:"))
+number2=int(input("Enter Second Number:"))
+
+print("Addition=",number1+number2)
+print("Substraction=",number1-number2)
+print("Multipication=",number1*number2)
+print("Dvision=",number1/number2)
+
+print("Thanku for using CALCULATOR")
+
