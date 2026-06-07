e9fc26f749cae8ebe3a723b46e38047b91a81deb
Sakshi Shelke <saakshi.shelke@gmail.com>
2026-06-07 12:25:59 +0530

    Add files via upload

---
+++ dream_companies_dict.py
@@ -0,0 +1,6 @@
+companies={"first":"Google","second":"Microsoft","third":"OpenAI"}
+
+print("my first dream company is:",companies["first"])
+print("my second dream company is:",companies["second"])
+print("my third dream company is :",companies["third"])
+
