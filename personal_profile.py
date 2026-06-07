009ce19763fcf23e9e8580db790681a358859006
Sakshi Shelke <saakshi.shelke@gmail.com>
2026-06-07 12:25:35 +0530

    Add files via upload

---
+++ personal_profile.py
@@ -0,0 +1,7 @@
+profile={"name":"sakshi","age":22,"city":"pune","Dream_Company":"Google","Favorite_language":"python"}
+
+print(profile["name"])
+print(profile["age"])
+print(profile["city"])
+print(profile["Dream_Company"])
+print(profile["Favorite_language"])
